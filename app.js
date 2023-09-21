6edce043f8ea8a9b8181c850c0a4f1446792550e
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-09-21 11:14:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require ('express')
+const app = express()
+
+const path = require('path');
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'static'))); // o 'static'
+
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    // Procedimiento 4:
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
+})
+
+app.get('/chat', (req, resp) => {
+    resp.sendFile(`${__dirname}/cliente/index.html`)
+})
+
+app.get('/code', (req, resp) => {
+    resp.sendFile(`${__dirname}/cliente/code_editor.html`)
+})
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${__dirname}/cliente/chat_view.html`)
+})
+
+server.listen(3000,() => {
+    console.log('Servidor corriendo en http://localhost:3000')
+})
